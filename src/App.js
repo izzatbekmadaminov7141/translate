b02220ddf5019a7f89d7491e30615d777145db9b
izzatbekmadaminov7141 <izzatbekmadaminov7141@gmail.com>
2023-03-21 03:26:06 +0500

    trasnlate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t, i18n } = useTranslation();
+  function handleClick(lang) {
+    i18n.changeLanguage(lang);
+  }
   return (
     <div className="App">
+      <nav style={{ width: "100%", padding: "2rem 0", background: "grey" }}>
+        <button onClick={() => handleClick("en")}>en</button>
+        <button onClick={() => handleClick("uz")}>uz</button>
+        <button onClick={() => handleClick("ru")}>ru</button>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{t("Thanks.1")}</p>
+        <p>{t("why.1")}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
